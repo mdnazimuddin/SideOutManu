f044edef5afffa13594f49d0c15cd44109203eab
iMac <imac@iMacs-iMac.local>
2018-05-10 00:28:24 +0600

    Total SlideOutManu Project

--- SlideOutManu/ViewController.swift
+++ SlideOutManu/ViewController.swift
@@ -10,16 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var sideBarManuOut: NSLayoutConstraint!
+    var isSideBarHidden = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // ininsialize constent -140
+        sideBarManuOut.constant = -140
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func organizeBtn(_ sender: UIBarButtonItem) {
+        if isSideBarHidden {
+            sideBarManuOut.constant = 0
+            UIView.animate(withDuration: 0.5, animations: {
+                self.view.layoutIfNeeded()
+                
+            })
+        }else{
+            sideBarManuOut.constant = -140
+            UIView.animate(withDuration: 0.5, animations: {
+                self.view.layoutIfNeeded()
+            })
+        }
+        isSideBarHidden = !isSideBarHidden
     }
-
+    
 
 }
 
